12c9e662c5fe551e03f5612de120552cbb60ffe8
AbilityG <tursumbekov@mail.ru>
2018-08-14 10:15:32 +0600

    exhib

---
+++ js/common.js
@@ -0,0 +1,11 @@
+$(function() {
+
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow: '<span class="slick-prev"></span>',
+    nextArrow: '<span class="slick-next"></span>',
+  });
+
+});
